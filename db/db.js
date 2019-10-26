85b1953ede130f75eac5c8e75aac883de6897285
Tim Malstead <timmmalstead@gmail.com>
2019-10-26 16:17:54 -0700

    set ports and database

---
+++ db/db.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose")
+const connection = "mongodb://localhost/veryimportantdatabase"
+
+mongoose.connect(connection, {
+    useNewUrlParser : true,
+    useUnifiedTopology : true,
+    useCreateIndex : true,
+    useFindAndModify : false
+})
+
+mongoose.connection.on(`connected`, () => {
+    console.log(`mongoose connected on ${connection}`)
+})
+
+mongoose.connection.on(`disconnected`, () => {
+    console.log(`mongoose has disconnected`)
+    mongoose.disconnect
+})
+
+mongoose.connection.on(`error`, (err) => {
+    console.log(`mongoose has logged the following error: ${err}`)
+    mongoose.disconnect
+})

75743873bd0299a2a700014038a975ec4cbc1f6c
Tim Malstead <timmmalstead@gmail.com>
2019-10-30 14:00:49 -0700

    create randomwords function

---
+++ public/js/randomWords.js
@@ -0,0 +1,13 @@
+// console.log("HOW-DEE!")
+
+const bottomButton = document.querySelector(".bottombutton")
+
+bottomButton.addEventListener("click", () => {
+    console.log("HOW-DEE!")
+})
+
+// lightDark.addEventListener("click", function() {
+//     document.documentElement.classList.toggle("inverted")
+//     document.body.classList.toggle("dark")
+//     tom.classList.toggle("inverted")
+// })   
